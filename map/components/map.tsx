16dc47b303c344ee5b7138fc6920196ac06ba0b7
shelby <53214352+shelbygreen@users.noreply.github.com>
2022-10-27 18:04:44 -0400

    refreshed data and did general layout (#2)

--- map/components/map.tsx
+++ map/components/map.tsx
@@ -31,8 +31,25 @@ const Map = () => {
       style: "mapbox://styles/mapbox/dark-v10",
       center: [-84.2830590599657, 30.43936603257657],
       interactive: true,
-      zoom: 12
+      zoom: 16,
+      minZoom: 8
     });
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Add geolocate control to the map.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true
+      })
+      );
 
     // load layer
     map.on("load", function () {
@@ -51,34 +68,69 @@ const Map = () => {
         source: "points",
         layout: {},
         paint: {
-            'circle-radius': {
-                'base': 1.75,
-                'stops': [
-                [12, 2],
-                [22, 180]
-                ]
-            },
-            'circle-color': '#fbb03b',
-            "circle-stroke-color": "#5d765f",
-            "circle-stroke-width": 2
+            'circle-radius': [
+              "interpolate",
+              ["linear"],
+              ["get", "amount"],
+              1, 2,
+              100, 4,
+              250, 7,
+              500, 10,
+              1000, 15,
+            ],
+            // {
+            //     'base': 1.75,
+            //     'stops': [
+            //     [12, 2],
+            //     [22, 180]
+            //     ]
+            // },
+            'circle-color': [
+              'match',
+              ['get', 'cont. type'],
+              'I', // individual
+              '#E09F7D',
+              'B', // business
+              '#311847',
+              'S', // self
+              '#A01A7D', // 51 to 75
+              'F', // political committee
+              '#EF5D60', // 76 to 99
+              'C',
+              '#EC4067', // committee
+              '#ccc', // other
+          ],
+          "circle-stroke-color": "#fff",
+          "circle-stroke-width": 1
         },
       });
     });
 
-    // const map = new mapboxgl.Map({
-    //     container: mapContainer.current,
-    //     style: `mapbox://styles/shelby-green/ckpe45kll0we417n7cgs8cxne`,
-    //     center: [-1.14, -0.98],
-    //     minZoom: 2,
-    //     interactive: false
-    // })
+    // after the map loads
+    map.on('click', function(mapElement) {
+      const features = map.queryRenderedFeatures(mapElement.point, {
+        layers: ['points-circle']
+      })
 
-    // mapRef.current = map
+      const name = features[0].properties.name
+      const amount = features[0].properties.amount
+
+      const html = `${name} donated $${amount}`
+
+      // create tooltip variable for the floating card div
+      const tooltip = document.getElementById('popup')
+            
+      // store html in the tooltip, which will be displayed in the floating card div
+      tooltip.innerHTML = html
+
+    });
+
   }, []);
 
   return (
     <div>
-      <div ref={mapNode} style={{ height: "45vh" }} />
+      <div id="popup">Select a dot to display donation details.</div>
+      <div ref={mapNode} style={{ height: "50vh" }} />
     </div>
   );
 };
--- map/pages/dailey.tsx
+++ map/pages/dailey.tsx
@@ -11,53 +11,48 @@ const Home: NextPage = () => {
     
     <div className={styles.container}>
       <Head>
-        <meta name="description" content="Politician profile for John Dailey" />
+        <meta name="description" content="Map of John Dailey's campaign contributions" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* header */}
-      <div className=" border-b-2 border-slate-900 ">
-        <div className={styles.heading}>Open Tallahassee</div>
-      </div>
+      <a href="/">
+        <div className=" mt-1 text-bold text-2xl text-green-900 text-left sm:text-lg sm:text-left sm:mt-1 ">Open Tallahassee</div>
+      </a>
       
       {/* top section */}
       <div>
-        <div className=" group flex items-center ">
-          <div className=" flex m-1 h-32 w-32 rounded-full border-4 border-slate-900 sm:w-24 sm:h-24 rounded-full border-b-4 border-slate-900">
+        <div className=" group flex-wrap justify-center sm: group flex ">
+          <div className=" flex mt-2 mb-2 mr-5 h-56 w-56 rounded-full border-2 border-slate-900 sm:w-56 sm:h-56 rounded-full border-b-2 border-slate-900">
             <Image className=" overflow-hidden rounded-full" alt="" src={pic} />
           </div>
-          <div className=" ltr:ml-3 ">
-            <button className=" bg-cyan-500 text-white font-bold py-2 px-4 rounded-full ">John Dailey</button>
-            <p className="text-sm font-medium text-slate-900 ">Currently: Mayor of Tallahassee</p>
-            <p className="text-sm font-medium text-slate-900 ">Running for re-election, against Kristin Dozier</p>
+          <div>
+            <div className=" mt-5 text-4xl text-slate-900 text-center sm:text-4xl sm:text-left sm:mt-20">John Dailey</div>
+            <div className=" text-xl text-slate-900 text-center sm: text-4xl sm:text-left">Currently: Mayor of Tallahassee</div>
+            <div className=" text-xl text-slate-900 text-center sm: text-4xl sm:text-left">Running for re-election, against Kristin Dozier</div>
           </div>
         </div>
-      <h1 className="text-sm text-center mt-10 font-bold text-grey-900 sm:text-lg ">Raised $_______ from _______ donors for this race (as of 10/24/22):</h1>
-        {/* <div className=" m-1 h-24 w-24 flex justify-center sm:w-24 sm:h-24 rounded-full flex justify-center">
-          <Image
-          src={pic}
-          alt="Picture of John Dailey"
-          className="rounded-full" 
-          />
-        </div>
-        <h1 className="text-2xl text-center font-bold text-grey-900 sm:text-2xl ">John Dailey</h1>
-        <h1 className="text-sm text-center font-bold text-grey-900 sm:text-lg ">Currently: Mayor of Tallahassee</h1>
-        <h1 className="text-sm text-center font-bold text-grey-900 sm:text-lg ">Running for re-election, against Kristin Dozier</h1>
-        <h1 className="text-sm text-center mt-10 font-bold text-grey-900 sm:text-lg ">Raised $_______ from _______ donors for this race (as of 10/24/22):</h1> */}
+      <div className="text-lg text-center mt-10 text-grey-900 sm:text-4xl ">Raised $449,683.44 from 867 individual donors (as of 10/27/22):</div>
       </div>
 
       {/* middle section */}
-      <div className=" bg-red-200 mt-5">
+      <div className=" mt-5 text-xl text-center sm: text-lg">
           <Map/> 
+          {/* legend */}
+          <div className="group flex">
+            <div className="group flex items-center">
+              <span className="w-10 h-10 inline-block bg-tumbleweed-500 rounded-md m-2"></span>
+              <span className="text-gray-600 dark:text-gray-400">Legend indicator</span>
+            </div>
+            <div className="group flex items-center">
+              <span className="w-2 h-2 inline-block bg-gray-500 rounded-full mr-2"></span>
+              <span className="text-gray-600 dark:text-gray-400">Legend indicator</span>
+            </div>
+          </div>
       </div>
-
-    {/* bottom section */}
-    <div className=" bg-red-200 ">
-      Search contributions by name or address:
-    </div>
 
     {/* footer */}
     <footer className={styles.footer}>
-      <a href="/">Open Tallahassee</a> is a digital publication that makes it easier to stay informed and connected in Tallahassee.
+      Built by <a href="https://www.shelbygn.com">Shelby Green</a>. This is a proof of concept for <a href="https://www.opentlh.com">Open Tallahassee</a>. Methodology <a href="https://github.com/Open-Data-Tallahassee/campaign-finance">here</a>. Have suggestions or feedback? <a href="mailto:opentlh@gmail.com">Email us</a>.
     </footer>
     </div>
     </div>
